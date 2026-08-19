94b86e33da759b91825290e88eba414b27eadf3c
cyvoriq01solutions-coder <cyvoriq01solutions@gmail.com>
2026-08-19 14:44:45 +0530

    ui: load commercial info page styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import App from "./App";
 import "./styles/global.css";
+import "./styles/info-pages.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
