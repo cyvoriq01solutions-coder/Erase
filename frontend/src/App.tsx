183e97987f17da35de5323daac849502bf480ffe
cyvoriq01solutions-coder <cyvoriq01solutions@gmail.com>
2026-08-19 08:56:34 +0530

    frontend: add commercial public routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import PublicLayout from "./layouts/PublicLayout";
 import AppLayout from "./layouts/AppLayout";
 import HomePage from "./pages/HomePage";
 import InfoPage from "./pages/InfoPage";
+import DownloadPage from "./pages/DownloadPage";
 import DashboardPage from "./pages/DashboardPage";
 import NotFoundPage from "./pages/NotFoundPage";
 
@@ -11,12 +12,18 @@ export default function App() {
     <Routes>
       <Route element={<PublicLayout />}>
         <Route path="/" element={<HomePage />} />
-        <Route path="/platform" element={<InfoPage title="Platform" />} />
+        <Route path="/why-cyvra" element={<InfoPage title="Why CYVRA" />} />
         <Route path="/how-it-works" element={<InfoPage title="How It Works" />} />
-        <Route path="/assurance" element={<InfoPage title="Assurance" />} />
-        <Route path="/security" element={<InfoPage title="Security" />} />
+        <Route path="/dpdp-readiness" element={<InfoPage title="DPDP Readiness" />} />
+        <Route path="/individuals" element={<InfoPage title="For Individuals" />} />
+        <Route path="/enterprise" element={<InfoPage title="Enterprise & OEM" />} />
         <Route path="/resources" element={<InfoPage title="Resources" />} />
         <Route path="/contact" element={<InfoPage title="Contact" />} />
+        <Route path="/download" element={<DownloadPage />} />
+
+        <Route path="/platform" element={<InfoPage title="Platform" />} />
+        <Route path="/assurance" element={<InfoPage title="Assurance" />} />
+        <Route path="/security" element={<InfoPage title="Security" />} />
       </Route>
 
       <Route path="/app" element={<AppLayout />}>
